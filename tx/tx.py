160871ad2b58fbb3519fc90141fa93f70c0b8c70
Tian Siyuan <tiansiyuan@gmail.com>
2012-12-15 21:57:10 +0800

    tx related stuff

---
+++ tx/tx.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""to help tx ACS-DOCS"""
+
+import os
+import operator
+from pprint import pprint
+
+pathname = '/home/tian/translations'
+en_dict = dict()
+
+for i in os.listdir(pathname):
+    if os.path.isdir(pathname+'/'+i) and i[:9]=='ACS_DOCS.':
+        dirs = os.listdir(pathname+'/'+i)
+        if 'en.po' in dirs and 'zh_CN' not in dirs:
+           en_dict[ i[9:] ] = os.path.getsize(pathname+'/'+i+'/'+'en.po')
+
+sorted_en = sorted(en_dict.iteritems(), key=operator.itemgetter(1))
+pprint(sorted_en)
