a231e5edd1bab4c1b32ba0a8c305dba3bc29d865
Tian Siyuan <tiansiyuan@gmail.com>
2012-12-16 23:19:14 +0800

    revised tx.py and add txpush

--- tx/tx.py
+++ tx/tx.py
@@ -6,13 +6,13 @@ import os
 import operator
 from pprint import pprint
 
-pathname = '/home/tian/translations'
+pathname = '/home/tian/projects/csdocs/translations'
 en_dict = dict()
 
 for i in os.listdir(pathname):
     if os.path.isdir(pathname+'/'+i) and i[:9]=='ACS_DOCS.':
         dirs = os.listdir(pathname+'/'+i)
-        if 'en.po' in dirs and 'zh_CN' not in dirs:
+        if ('en.po' in dirs) and ('zh_CN.po' not in dirs):
            en_dict[ i[9:] ] = os.path.getsize(pathname+'/'+i+'/'+'en.po')
 
 sorted_en = sorted(en_dict.iteritems(), key=operator.itemgetter(1))
